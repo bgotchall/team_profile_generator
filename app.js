28ff8435853086d12b318595c43906d57f451f26
austinev <bgotchall@gmail.com>
2019-12-18 17:02:50 -0600

    CLI input now working witih promisifying approach

--- app.js
+++ app.js
@@ -4,116 +4,193 @@ const engineer_class = require("./lib/engineer.js");
 const intern_class = require("./lib/intern.js");
 const inquirer = require("inquirer");
 
-var this_manager = new manager_class();              //placeholders for later
-var this_engineer = new engineer_class();              
-var this_intern = new intern_class();              
+const util = require("util");
+
+
+
+var this_manager = new manager_class(); //placeholders for later
+var this_engineer = new engineer_class();
+var this_intern = new intern_class();
 
 const bob = new employee_class("Bob", 1, "bob@fakemail.com");
 const norman = new manager_class("Norman", 2, "norman@fakemail.com", 4564345);
 
-// bob.printathing();
-// console.log(bob.getName());
-// console.log(bob.getId());
-// console.log(bob.getEmail());
-// console.log(bob.getRole());
-
-// console.log(norman.getOfficeNumber());
-// console.log("hi");
-
-getOneEmployee();
-//console.log(employees);
 
 var employees = []; //array of employee objects
+var another_employee = true;
 
-function getOneEmployee() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is this employee's name?",
-        name: "name"
-      },
-      {
-        type: "number",
-        message: "What is this employee's Employee ID?",
-        name: "ID"
-      },
-      {
-        type: "input",
-        message: "What is this employee's email address?",
-        name: "email"
-      },
-      {
-        type: "list",
-        message: "What is this employee's role?",
-        name: "role",
-        choices: ["Manager", "Engineer", "Intern"]
-      },
-      {
-        type: "confirm",
-        message: "Add another employee?",
-        name: "not_done"
-      }
-    ])
-    .then(function(response) {
-     
-      switch (response.role) {
-         
-        case "Manager":
-            this_manager =new manager_class(response.name, response.ID, "norman@fakemail.com", 4564345);
-            employees.push(this_manager);
-          break;
-        case "Engineer":
-            this_engineer = new engineer_class(response.name, response.ID, "norman@fakemail.com", "gitstub");
-            employees.push(this_engineer);
-          break;
-        case "Intern":
-            this_intern = new intern_class(response.name, response.ID, "norman@fakemail.com", "schoolstub");
-            employees.push(this_intern);          
-        break;
+async function init() {
 
-        default:
-          break;
-      }
+    while (another_employee) {
 
-      
+        try {
+            const answers = await getOneEmployee();
 
-      if (response.not_done) {
-        getOneEmployee();
-      } else {
-        //data input is done here.
-        
-        console.log("inputs done.  showing the first name here: ");
-        console.log(employees[1].getName());
+///////////////////////////////
+            
 
-      }
-    });
+            switch (answers.role) {
+                case "Manager":
+                    const officePhone = await getOneAnswer("What is this employee's Office Phone #?");
+                    this_manager = new manager_class(answers.name, answers.ID, answers.email, officePhone.answer);
+                    employees.push(this_manager);
+                    break;
+                case "Engineer":
+                    const github = await getOneAnswer("What is this employee's github?");
+                    this_engineer = new engineer_class(answers.name, answers.ID, answers.email, github.answer);
+                    employees.push(this_engineer);
+                    break;
+                case "Intern":
+                    const school = await getOneAnswer("What is this employee's school?");
+                    this_intern = new intern_class(answers.name, answers.ID, answers.email, school.answer);
+                    employees.push(this_intern);
+                    break;
+                default:
+                    break;
+            }
+/////////////////////////////////
+            const answer3 = await promptToContinue("Would you like to enter another employee?");
+
+        //     console.log(`name was: ${answers.name}`);
+        //    // console.log(`alien was: ${answer2.answer}`);
+        //     console.log(`another was: ${answer3.answer}`);
+
+            another_employee = answer3.answer;
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+//const getInputsAsync = util.promisify(init);
+
+async function writeTeamFile() {
+
+    try {
+        const nothing = await init();
+        console.log("Successfully wrote to index.html");
+        console.log(`second name is ${employees[1].getName()}`);
+    } catch (error) {
+
+    }
 }
 
-// inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       message: "What is yourname?",
-//       name: "username"
-//     },
-//     {
-//       type: "password",
-//       message: "What is your password?",
-//       name: "password"
-//     },
-//     {
-//       type: "password",
-//       message: "Re-enter password to confirm:",
-//       name: "confirm"
-//     }
-//   ])
-//   .then(function(response) {
+writeTeamFile();
 
-//     if (response.confirm === response.password) {
-//       console.log("Success!");
-//     }
-//     else {
-//       console.log("You forgot your password already?!");
-//     }
-//   });
+function getOneEmployee() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is this employee's name?",
+            name: "name"
+        },
+        {
+            type: "number",
+            message: "What is this employee's Employee ID?",
+            name: "ID"
+        },
+        {
+            type: "input",
+            message: "What is this employee's email address?",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "What is this employee's role?",
+            name: "role",
+            choices: ["Manager", "Engineer", "Intern"]
+        }
+    ]);
+};
+
+
+function getOneAnswer(Prompt) {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: Prompt,
+            name: "answer"
+        }
+    ]);
+};
+
+function promptToContinue(Prompt) {
+    return inquirer.prompt([
+        {
+            type: "confirm",
+            message: Prompt,
+            name: "answer"
+        }
+    ]);
+};
+
+
+function getOneEmployee_old() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is this employee's name?",
+                name: "name"
+            },
+            {
+                type: "number",
+                message: "What is this employee's Employee ID?",
+                name: "ID"
+            },
+            {
+                type: "input",
+                message: "What is this employee's email address?",
+                name: "email"
+            },
+            {
+                type: "list",
+                message: "What is this employee's role?",
+                name: "role",
+                choices: ["Manager", "Engineer", "Intern"]
+            },
+            {
+                type: "confirm",
+                message: "Add another employee?",
+                name: "not_done"
+            }
+        ])
+        .then(function (response) {
+            switch (response.role) {
+                case "Manager":
+                    this_manager = new manager_class(response.name, response.ID, "norman@fakemail.com", 4564345);
+                    employees.push(this_manager);
+                    break;
+                case "Engineer":
+                    this_engineer = new engineer_class(
+                        response.name,
+                        response.ID,
+                        "norman@fakemail.com",
+                        "gitstub"
+                    );
+                    employees.push(this_engineer);
+                    break;
+                case "Intern":
+                    this_intern = new intern_class(
+                        response.name,
+                        response.ID,
+                        "norman@fakemail.com",
+                        "schoolstub"
+                    );
+                    employees.push(this_intern);
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (response.not_done) {
+                getOneEmployee();
+            } else {
+                //data input is done here.
+
+                console.log("inputs done.  showing the first name here: ");
+                console.log(employees[1].getName());
+            }
+        });
+}
